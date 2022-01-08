94601bcd52008445bb45cfceb226e368c4534085
Kyle Gray <kylemaxgray@gmail.com>
2022-01-08 10:50:53 -0800

    more date and time js katas

--- dateTime.js
+++ dateTime.js
@@ -6,11 +6,28 @@ const myDate = new Date();
 // create options for how to display the date
 // crazy american format mm-dd-yy 
 console.log('American: ', new Intl.DateTimeFormat('en-US').format(myDate));
+// American:  1/8/2022
 
 // more sane british format dd-mm-yy
 console.log('British: ', new Intl.DateTimeFormat('en-GB').format(myDate));
+// British:  08/01/2022
 
 // using options
 // "long" for words
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 console.log('British Long:', new Intl.DateTimeFormat('en-GB', options).format(myDate));
+// British Long: Saturday, 8 January 2022
+
+// using UTC and disclosing timezone
+options.timeZone = 'UTC';
+options.timeZoneName = 'short';
+console.log('with time zone:', new Intl.DateTimeFormat('en-GB', options).format(myDate));
+// with time zone: Saturday, 8 January 2022, UTC
+
+// 24 hour / military time 
+options = { 
+  year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', 
+  minute: 'numeric', second: 'numeric', hour12: false, timeZone: 'America/Vancouver'
+};
+console.log('24 hour date and time: ', new Intl.DateTimeFormat('en-GB', options).format(myDate));
+// 24 hour date and time:  08/01/2022, 10:43:34
