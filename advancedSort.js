c070edafca5f20cb33d72a44d8d10df1b8848ae0
Kyle Gray <kylemaxgray@gmail.com>
2022-01-16 23:36:00 -0800

    create advanceSort file

---
+++ advancedSort.js
@@ -0,0 +1,10 @@
+const students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort((firstItem, secondItem) => firstItem.name - secondItem.name)
+
+console.log(students)
