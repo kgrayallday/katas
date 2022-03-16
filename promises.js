aeb6b24538962334335052fd3f5f6c8b21394e37
Kyle Gray <kylemaxgray@gmail.com>
2022-03-15 22:00:27 -0700

    add promise example code to file

--- promises.js
+++ promises.js
@@ -1,3 +1,45 @@
 // The Promise object represents the eventual completion (or failure)
 // of an asynchronous operation and its resulting value.
+// 
+// a promise is in one of three states
+//   1) pending
+//   2) fulfilled
+//   3) rejected
+//
+// Promise basics: 
 
+let myNewPromise = new Promise((resolve, reject) => {
+  let one = 1
+  if (one == 1) {
+    resolve('promise resoved!')
+  }
+  reject('promise rejected')
+})
+
+myNewPromise.then((message) => {
+  console.log('then we get a message:', message)
+}).catch(()=>{
+  console.log('catch the failure: ', message)
+})
+
+
+
+// Promise example with reading files
+const fs = require('fs')
+
+let myPromise = new Promise((resolve, reject) => {
+  fs.readFile('./test.txt', 'utf8', (error, data) => {
+    if (error) {
+      reject(error)
+    }
+    if (data) {
+      resolve(data)
+    }
+  })
+})
+
+myPromise.then((message) => {
+  console.log('all went well:', message)
+}).catch((message) => {
+  console.log(message)
+})
